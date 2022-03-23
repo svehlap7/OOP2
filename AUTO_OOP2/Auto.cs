40afe8de69da6f3f51a27a30268c089b08c1903e
brpgrrtygng <74527999+svehlap7@users.noreply.github.com>
2022-03-23 23:35:34 +0100

    Add files via upload

---
+++ AUTO_OOP2/Auto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUTO_OOP2
+{
+    internal class Auto
+    {
+        private string znacka;
+        private double spotreba;
+        private int ujetoCelkem = 0;
+        private DateTime okamzikRozjezdu;
+        private double dobaJizdyCelkem;
+
+        public bool Jede
+        { 
+            get;
+            private set;
+        }
+
+
+        public  Auto(string znacka, double spotreba)
+        {
+            this.znacka = znacka;
+            this.spotreba = spotreba;
+            ujetoCelkem = 0;
+            dobaJizdyCelkem = 0;
+            Jede = false;
+        }
+        public void Rozjed()
+        {
+            Jede = true;
+            okamzikRozjezdu = DateTime.Now;
+        }
+        public void Zastav(int km)
+        {
+            if (Jede)
+            {
+                ujetoCelkem += km;
+                dobaJizdyCelkem += (DateTime.Now - okamzikRozjezdu).TotalMinutes;
+            }
+            Jede = false;
+        }
+        public int Vrat()
+        {
+            return ujetoCelkem;
+        }
+        public double CelkovaSpotreba()
+        {
+            return ujetoCelkem / 100 * spotreba;
+        }
+        public override string ToString()
+        {
+            return string.Format("Auto: {0}, ma ujeto: {1}, doba jizdy je: {2}, celkova spotreba je: {3}.", znacka, Vrat(), dobaJizdyCelkem, CelkovaSpotreba());
+        }
+
+    }
+}
---
+++ AUTO_OOP2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AUTO_OOP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Auto auto;
+        public void Form1_Load(object sender, EventArgs e)
+        {
+           auto = new Auto("Scala", 5);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            auto = new Auto(textBox1.Text, Convert.ToInt32(textBox2.Text));
+            auto.Rozjed();
+            MessageBox.Show(auto.ToString());
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            auto = new Auto(textBox1.Text, Convert.ToInt32(textBox2.Text));
+            auto.Zastav(10);
+            MessageBox.Show(auto.ToString());
+        }
+
+       
+    }
+}
